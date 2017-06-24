09c35cfdbc3abbf898be22d7882542cc878dca65
Zan Fras <zan.fraas@gmail.com>
2017-06-24 19:48:58 +0200

    Test GET /todos/:id routes

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 
 //Midlewares
@@ -38,12 +39,16 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
 
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
     Todo.findById(id).then((todo) => {
         if (!todo) {
             res.status(404).send({});
         }
 
-        res.send(todo);
+        res.send({todo});
     }).catch((err) => {
         res.status(400).send({});
     });
