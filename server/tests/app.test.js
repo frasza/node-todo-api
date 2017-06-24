b1248d0b9ca3784491e17d31bd3bf05f12b107a2
Zan Fras <zan.fraas@gmail.com>
2017-06-24 20:41:26 +0200

    Test DELETE /todos/:id route

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -108,3 +108,43 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var haxId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${haxId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(haxId)
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(haxId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+
+                }).catch((err) => done(err));
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var haxId = new ObjectID().toHexString();
+
+        request(app)
+            .delete(`/todos/${haxId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if object id is invalid', (done) => {
+        request(app)
+            .delete('/todos/123abc')
+            .expect(404)
+            .end(done);
+    });
+});
