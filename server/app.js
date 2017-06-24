c9f57b46807e09a4ae4b11030e656cc0a4b5a738
Zan Fras <zan.fraas@gmail.com>
2017-06-24 20:27:08 +0200

    Add REMOVE /todos/:id route

--- server/app.js
+++ server/app.js
@@ -46,12 +46,31 @@ app.get('/todos/:id', (req, res) => {
 
     Todo.findById(id).then((todo) => {
         if (!todo) {
-            res.status(404).send({});
+            res.status(404).send();
         }
 
         res.send({todo});
     }).catch((err) => {
-        res.status(400).send({});
+        res.status(400).send();
+    });
+});
+
+// REMOVE /todos/:id
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send();
     });
 });
 
