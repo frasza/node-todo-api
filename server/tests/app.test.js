30dfe90e0751da1e6f8df7b455b0d5ec1f37de4e
Zan Fras <zan.fraas@gmail.com>
2017-06-25 10:46:11 +0200

    Test PATCH /todos/:id route

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -10,7 +10,9 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }];
 
 // Empty database
@@ -93,10 +95,10 @@ describe('GET /todos/:id', () => {
     });
 
     it('should return 404 if todo not found', (done) => {
-        var haxId = new ObjectID().toHexString();
+        var hexId = new ObjectID().toHexString();
 
         request(app)
-            .get(`/todos/${haxId}`)
+            .get(`/todos/${hexId}`)
             .expect(404)
             .end(done);
     });
@@ -111,20 +113,20 @@ describe('GET /todos/:id', () => {
 
 describe('DELETE /todos/:id', () => {
     it('should remove a todo', (done) => {
-        var haxId = todos[1]._id.toHexString();
+        var hexId = todos[1]._id.toHexString();
 
         request(app)
-            .delete(`/todos/${haxId}`)
+            .delete(`/todos/${hexId}`)
             .expect(200)
             .expect((res) => {
-                expect(res.body.todo._id).toBe(haxId)
+                expect(res.body.todo._id).toBe(hexId)
             })
             .end((err, res) => {
                 if (err) {
                     return done(err);
                 }
 
-                Todo.findById(haxId).then((todo) => {
+                Todo.findById(hexId).then((todo) => {
                     expect(todo).toNotExist();
                     done();
 
@@ -133,10 +135,10 @@ describe('DELETE /todos/:id', () => {
     });
 
     it('should return 404 if todo not found', (done) => {
-        var haxId = new ObjectID().toHexString();
+        var hexId = new ObjectID().toHexString();
 
         request(app)
-            .delete(`/todos/${haxId}`)
+            .delete(`/todos/${hexId}`)
             .expect(404)
             .end(done);
     });
@@ -145,6 +147,46 @@ describe('DELETE /todos/:id', () => {
         request(app)
             .delete('/todos/123abc')
             .expect(404)
+            .end(done);
+    });
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        var text = 'This should be the new text';
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: true,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                expect(res.body.todo.completedAt).toBeA('number');
+            })
+            .end(done);
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        var hexId = todos[1]._id.toHexString();
+        var text = 'This should be the new text!!!';
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toNotExist();
+            })
             .end(done);
     });
 });
